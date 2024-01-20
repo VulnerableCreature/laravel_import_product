f62073b01ea0653d32826cbc39e04cf1cdcc016b
VulnerableCreature <otselsiy@mail.ru>
2024-01-20 17:05:09 +0300

    feat: add table `photo`

---
+++ src/database/migrations/2024_01_20_140311_create_table_photo_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_photo_products', function (Blueprint $table) {
+            $table->id();
+            /*-----------------Foreign key---------------*/
+            $table->integer('product_id')->comment("Внешний ключ для связи с таблицей products");
+            $table->index('product_id', 'product_photo_idx');
+            $table->foreign('product_id', 'product_photo_fk')->references('product_id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+            /*-------------------------------------------*/
+            $table->string('path')->comment("Путь к фотографии");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_photo_products');
+    }
+};
